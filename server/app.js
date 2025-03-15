de53cac97a880810df2530e283f13dd03d8dee80
Bhagwan Singh <bhagwan.5602@gmail.com>
2025-03-15 21:14:24 +0530

    fix env

--- server/app.js
+++ server/app.js
@@ -1,23 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
+
 const app = express();
 
-// Set EJS as templating engine and define views directory
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Serve static files from 'public' directory
+/** Serve static files from 'assets' directory */
 app.use(express.static(path.join(__dirname, '../', 'assets')));
 
-// Define your routes
 app.get('/', (req, res) => {
     // Render the 'index.ejs' view (you can also pass dynamic data)
     res.render('index', { title: 'Home Page' });
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port http://127.0.0.1:${PORT}`);
 });
