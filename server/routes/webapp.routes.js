8fa26251c2139e232d4dc9295b9eb72cd521f6e5
Bhagwan Singh <bhagwan.5602@gmail.com>
2025-03-15 21:35:13 +0530

    add routes

--- server/routes/webapp.routes.js
+++ server/routes/webapp.routes.js
@@ -15,6 +15,16 @@ router.get("/", async (req, res) => {
     res.render('index', context);
 });
 
+router.get("/about", async (req, res) => {
+    // Render the 'index.ejs' view (you can also pass dynamic data)
+
+    const context = {
+        title: 'About Us'
+    };
+
+    res.render('about', context);
+});
+
 
 
 export default router;
