9279cde6ae9d415619f59994122ebdc98b14ec75
Bhagwan Singh <bhagwan.5602@gmail.com>
2025-03-16 19:45:10 +0530

    add routes

--- server.js
+++ server.js
@@ -25,6 +25,65 @@ app.get("/about", async (req, res) => {
     res.render('about', context);
 });
 
+
+app.get("/career", async (req, res) => {
+    console.log("=>>> service career page ::: ")
+    const context = {
+        title: 'Home Page'
+    };
+    res.render('career', context);
+});
+
+app.get("/contact", async (req, res) => {
+    console.log("=>>> service contact page ::: ")
+    const context = {
+        title: 'Home Page'
+    };
+    res.render('contact', context);
+});
+
+app.get("/establishment", async (req, res) => {
+    console.log("=>>> service establishment page ::: ")
+    const context = {
+        title: 'Home Page'
+    };
+    res.render('establishment', context);
+});
+
+app.get("/events", async (req, res) => {
+    console.log("=>>> service events page ::: ")
+    const context = {
+        title: 'Home Page'
+    };
+    res.render('events', context);
+});
+
+app.get("/news", async (req, res) => {
+    console.log("=>>> service news page ::: ")
+    const context = {
+        title: 'Home Page'
+    };
+    res.render('news', context);
+});
+
+app.get("/particle-wave", async (req, res) => {
+    console.log("=>>> service particle-wave page ::: ")
+    const context = {
+        title: 'Home Page'
+    };
+    res.render('particle-wave', context);
+});
+
+app.get("/test", async (req, res) => {
+    console.log("=>>> service test page ::: ")
+    const context = {
+        title: 'Home Page'
+    };
+    res.render('test', context);
+});
+
+
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Vexon Server is running on port http://127.0.0.1:${PORT}`);
